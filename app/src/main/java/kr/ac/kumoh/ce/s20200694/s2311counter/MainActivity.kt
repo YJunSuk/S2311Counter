b7791110b9180e11cf35513930b54caea2b4bccd
JS-Yang <junsuk530@gmail.com>
2023-11-16 17:25:20 +0900

    🏗️ ViewModel 사용

--- app/src/main/java/kr/ac/kumoh/ce/s20200694/s2311counter/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200694/s2311counter/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -26,10 +27,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kr.ac.kumoh.ce.prof01.s23w1102counter.CounterViewModel
 import kr.ac.kumoh.ce.s20200694.s2311counter.ui.theme.S2311CounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val vm = ViewModelProvider(this)[CounterViewModel::class.java]
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
@@ -40,8 +45,8 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                 ) {
                     //Clicker()
-                    Counter()
-                    Counter()
+                    Counter(vm)
+                    Counter(vm)
                 }
 
             }
@@ -87,10 +92,11 @@ fun Clicker(){
 }
 
 @Composable
-fun Counter(){
+fun Counter(viewModel: CounterViewModel){
     //var txtString by remember{ mutableStateOf("눌러주세요")}
-    val(count, setCount) = rememberSaveable {mutableStateOf(0)
-    }
+    //val(count, setCount) = rememberSaveable {mutableStateOf(0)
+    val count by viewModel.count.observeAsState(0)
+
     Column(modifier = Modifier
         .padding(8.dp),
         verticalArrangement = Arrangement.Center,
@@ -101,14 +107,14 @@ fun Counter(){
         Row {
             Button(modifier = Modifier.weight(1f),
                 onClick = {
-                    setCount(count + 1)
+                    viewModel.onAdd()
                 }) {
                 Text("증가")
             }
             Spacer(modifier = Modifier.width(8.dp))
             Button(modifier = Modifier.weight(1f),
                 onClick = {
-                    setCount(count - 1)
+                    viewModel.onSub()
                 }) {
                 Text("감소")
             }
